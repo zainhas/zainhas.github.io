b00728be37c7e5c8cf5985e320086fc955193350
zainhas <zain_has@hotmail.com>
2025-08-02 08:32:28 +0000

    Deploying to gh-pages from @ zainhas/zainhas.github.io@b6ca1537a103b0de0ef1af06a0cee1bf72ee3e95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,13 +113,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/zainhas", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
